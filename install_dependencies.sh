655e950320033a4ef64f8f736d8c86b9db4e77b2
marcisd <blitboi303@gmail.com>
2026-04-29 11:20:18 +0300

    Changed deps

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,4 +5,21 @@ set -eu
 sudo apt update
 sudo apt install -y build-essential make libncurses-dev
 
-echo "Dependencies installed."
+# Install Raylib dependencies
+sudo apt install -y \
+    libx11-dev \
+    libxrandr-dev \
+    libxinerama-dev \
+    libxi-dev \
+    libxext-dev \
+    libxcursor-dev \
+    libgl1-mesa-dev \
+    libudev-dev
+
+# Install Raylib
+git clone https://github.com/raysan5/raylib.git /tmp/raylib
+cd /tmp/raylib/src
+make
+sudo make install
+
+echo "Dependencies installed, including Raylib."
